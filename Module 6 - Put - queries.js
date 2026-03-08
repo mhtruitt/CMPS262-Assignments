47908c3300f56285e78d406083c6138774065148
Morgan Truitt <mhtruit@pointpark.edu>
2026-03-08 09:46:59 -0400

    Create queries for video game inventory management
    Added SQL queries for video game inventory operations.

---
+++ Module 6 - Put - queries.js
@@ -0,0 +1,10 @@
+const getAccounts = "SELECT * FROM video_game_inventory";
+const getAccountsById = "SELECT * FROM video_game_inventory WHERE id = $1";
+const addNewAccount = "INSERT INTO video_game_inventory (game_title, developer, release_year, game_genre, ign_rating, list_type) VALUES ($1, $2, $3, $4, $5, $6)";
+const updateAccount = "UPDATE video_game_inventory SET game_title = $1, developer = $2, release_year = $3, game_genre = $4, ign_rating = $5, list_type = $6 WHERE id = $7";
+
+module.exports = {
+    getAccounts,
+    getAccountsById,
+    addNewAccount,
+};
