deff37acca4d7c99759fdeca5dbd735cae79429f
Morgan Truitt <mhtruit@pointpark.edu>
2026-03-08 09:34:42 -0400

    Add routes for account management

---
+++ M6 Post - routes.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const controller = require('./controller');
+
+const router = Router();
+router.get('/', controller.getAccounts);
+router.get('/accounts/:id', controller.getAccountsById);
+router.post('/accounts', controller.addNewAccount);
+
+
+module.exports = router;
